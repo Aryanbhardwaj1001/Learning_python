0b79ad2e8b395552705c7019ac322867a130bc81
aryanbhardwaj1001 <ravishankarsingh09490@gmail.com>
2023-08-27 21:31:43 +0530

    added comment

--- C_odd_even_function.py
+++ C_odd_even_function.py
@@ -1,4 +1,6 @@
+#this will ask your name and then your fav no. and tell you that it is odd or even
 print("what is your name?")
+#name is a variable to store the user name/input
 name = input()
 print("hello"+name)
 print("type your fav no")
